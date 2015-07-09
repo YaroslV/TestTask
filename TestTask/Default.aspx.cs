38cc227dea559d8d04e0b0f4592caa5801d9858e
YarosIV <yaroslav.voznij@gmail.com>
2015-07-09 10:37:59 +0300

    unused using removed

--- TestTask/Default.aspx.cs
+++ TestTask/Default.aspx.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 using System.Configuration;
 using System.Text;
 
